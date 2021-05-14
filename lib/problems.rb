c3bf43644bceea7bd1c3bdaaf2e0772d0be843ea
regine-tiu <regine.d.tiu@gmail.com>
2021-05-14 20:07:21 +0800

    solved

--- lib/problems.rb
+++ lib/problems.rb
@@ -36,7 +36,13 @@ end
 class Array
     # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
     def pair_sum_count(num)
-
+        count = 0
+        (0...self.length).each do |idx_1|
+            (idx_1 + 1 ...self.length).each do |idx_2|
+                count += 1 if self[idx_1] + self[idx_2] == num
+            end
+        end
+        count    
     end
 
     # Write a method, Array#bubble_sort, that takes in an optional proc argument.
@@ -53,6 +59,19 @@ class Array
     #
     # This should remind you of the spaceship operator! Convenient :)
     def bubble_sort(&prc)
+        prc ||= Proc.new{|a, b| (a <=> b)}
 
+       sorted = false
+       
+        while !sorted
+            sorted = true
+            (0...self.length - 1).each do |i|
+                if prc.call(self[i], self[i+1]) == 1
+                    sorted = false 
+                    self[i], self[i+1] = self[i+1], self[i]
+                end
+            end    
+        end  
+        self  
     end
 end
